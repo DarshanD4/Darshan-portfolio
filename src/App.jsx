98bc115970e15d87a13c7b0fd083928e503f127c
DarshanD4 <mpdbros@gmail.com>
2026-06-07 13:22:07 +0530

    Portfolio website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Experience from "./components/Experience";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import About from "./components/about";
+import Skills from "./components/skills";
+import Projects from "./components/projects";
+import Experience from "./components/experience";
+import Contact from "./components/contact";
+import Footer from "./components/footer";
 
 function App() {
   return (
